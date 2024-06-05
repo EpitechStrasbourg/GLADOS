c11b26afba5a80d7c4b287df73ce757215c77c9b
FornesLeo <leo1.fornes@epitech.eu>
2024-06-05 11:00:03 +0200

    feat/auth: /login command register user on db and send mail verification code

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -1,3 +1,6 @@
 export { default as Student } from "@/database/models/model.student"
+<<<<<<< HEAD
 
 export { default as Config } from "@/database/models/model.config"
+=======
+>>>>>>> a10dd36 (feat/auth: /login command register user on db and send mail verification code)
