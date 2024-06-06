f9763c5e611c6b9583df73cd540a620ca11e79cf
FornesLeo <leo1.fornes@epitech.eu>
2024-06-06 15:58:48 +0200

    fix/auth: fix rebase model database HEAD

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -1,9 +1,3 @@
 export { default as Student } from "@/database/models/model.student"
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 export { default as Config } from "@/database/models/model.config"
-=======
->>>>>>> a10dd36 (feat/auth: /login command register user on db and send mail verification code)
-=======
->>>>>>> c6b5c34c6888da4fef3e3303b524689a1be934fe
+
