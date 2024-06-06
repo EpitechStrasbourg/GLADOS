b069db323def33f17f6d910604d57e2531ce6cbc
FornesLeo <leo1.fornes@epitech.eu>
2024-06-06 15:17:11 +0200

    feat/auth: verification fetch login infos

--- src/commands/slash/verification.ts
+++ src/commands/slash/verification.ts
@@ -1,7 +1,4 @@
 import { Student } from "@/database/models"
-import isValidEpitechMail from "@/utils/isValidEpitechMail"
-import sendEmailToUser from "@/utils/mailer"
-import generateVerificationCode from "@/utils/verificationCode"
 
 import { SlashCommand, SlashCommandConfig } from "@/types/command"
 import { Logger } from "@/lib/logger"
@@ -27,13 +24,34 @@ const command: SlashCommand = {
     Logger.debug("info", `verificationCodeHandler called by ${interaction.user.id} with code ${code}`)
 
     try {
-      const student = await Student.findOne({ where: { verificationCode: code } })
-
+      const student = await Student.findOne({ where: { verificationCode: code }, raw: false })
+      Logger.debug("info", `verificationCodeHandler: ${interaction.user.id} with code ${code}, student ${JSON.stringify(student)}`)
       if (!student) {
         interaction.editReply("Code de vérification invalide.")
         return
       }
-      // verifier sur sauron les info de l'etudiant et lui attribuer les roles correspondants cc flo
+      const login = student.getDataValue("login")
+      Logger.debug("info", `${login}`)
+      const config = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `${process.env.SAURON_TOKEN}`
+        }
+      }
+      const response = await fetch(`https://api.sauron.epitest.eu/api/users/${login}/infos`, config)
+      Logger.debug("info", `verificationCodeHandler: ${interaction.user.id} with code ${code}, login ${login}, response: ${response.status} ${JSON.stringify(response)}`)
+      if (!response.ok) {
+        interaction.editReply(`Erreur lors de la vérification du code. fetch failed ${response.status}.`)
+        return
+      }
+      const data = await response.json()
+      Logger.debug("info", `verificationCodeHandler: ${interaction.user.id} with code ${code}, login ${login}, data: ${data}`)
+      if (data.error) {
+        interaction.editReply("Erreur lors de la vérification du code.")
+        return
+      }
+      // attribuer les roles
       interaction.editReply("Compte vérifié avec succès.")
     } catch (error) {
       Logger.error("error", `Error while verifying code: ${error}`)
