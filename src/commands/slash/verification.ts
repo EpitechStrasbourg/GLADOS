985590034a1ff300e68b74e95eea8e434260a3f5
Arthur ROBINE <arthur.robine@gmail.com>
2024-06-06 17:01:08 +0200

    feat: add promotion and student role after verification

--- src/commands/slash/verification.ts
+++ src/commands/slash/verification.ts
@@ -1,4 +1,5 @@
 import { Student } from "@/database/models"
+
 import { SlashCommand, SlashCommandConfig } from "@/types/command"
 import { Logger } from "@/lib/logger"
 
@@ -14,6 +15,36 @@ const config: SlashCommandConfig = {
     },
   ],
 }
+
+const mockData = {
+  login: "arthur.robine@epitech.eu",
+  cities: [
+    {
+      code: "FR/PAR",
+      name: "Paris",
+    },
+  ],
+  roles: ["student"],
+  promo: {
+    city: {
+      code: "FR/PAR",
+      name: "Paris",
+    },
+    cursus: {
+      code: "master",
+      name: "Cycle-expertise-et-innovation",
+    },
+    promotion_year: 2024,
+    subpromo: "classic",
+  },
+  is_active: true,
+  firstname: "Arthur",
+  lastname: "ROBINE",
+}
+
+const PGE_cycles = ["bachelor", "master"]
+const PGE_suffix = "PGE "
+const studentRoleName = "Étudiant"
 
 const command: SlashCommand = {
   execute: async (interaction) => {
@@ -39,6 +70,8 @@ const command: SlashCommand = {
       }
       const login = student.getDataValue("login")
       Logger.debug("info", `${login}`)
+
+      /*
       const config = {
         method: "GET",
         headers: {
@@ -46,61 +79,53 @@ const command: SlashCommand = {
           Authorization: `${process.env.SAURON_TOKEN}`,
         },
       }
-      // const response = await fetch(
-      //   `https://api.sauron.epitest.eu/api/users/${login}/infos`,
-      //   config
-      // )
-      // Logger.debug(
-      //   "info",
-      //   `verificationCodeHandler: ${interaction.user.id} with code ${code}, login ${login}, response: ${response.status} ${JSON.stringify(response)}`
-      // )
-      // if (!response.ok) {
-      //   interaction.editReply(
-      //     `Erreur lors de la vérification du code. fetch failed ${response.status}.`
-      //   )
-      //   return
-      // }
-      // const data = await response.json()
-      // Logger.debug(
-      //   "info",
-      //   `verificationCodeHandler: ${interaction.user.id} with code ${code}, login ${login}, data: ${data}`
-      // )
-      // if (data.error) {
-      //   interaction.editReply("Erreur lors de la vérification du code.")
-      //   return
-      // }
-    const data = {
-      "login": "tristan.distretti@epitech.eu",
-      "cities": [
-          {
-              "code": "FR/STG",
-              "name": "Strasbourg"
+      const response = await fetch(
+        `https://api.sauron.epitest.eu/api/users/${login}/infos`,
+        config
+      )
+      Logger.debug(
+        "info",
+        `verificationCodeHandler: ${interaction.user.id} with code ${code}, login ${login}, response: ${response.status} ${JSON.stringify(response)}`
+      )
+      if (!response.ok) {
+        interaction.editReply(
+          `Erreur lors de la vérification du code. fetch failed ${response.status}.`
+        )
+        return
+      }
+      const data = await response.json()
+      Logger.debug(
+        "info",
+        `verificationCodeHandler: ${interaction.user.id} with code ${code}, login ${login}, data: ${data}`
+      )
+      if (data.error) {
+        interaction.editReply("Erreur lors de la vérification du code.")
+        return
+      }
+      */
+
+      if (mockData.roles.includes("student")) {
+        const roles = await interaction.guild?.roles.fetch(undefined, {
+          force: true,
+        })
+        if (PGE_cycles.includes(mockData.promo.cursus.code) && roles) {
+          const roleName = PGE_suffix + mockData.promo.promotion_year.toString()
+          const guildRole = roles.find((r) => r.name === roleName)
+          const schoolRole = roles.find((r) => r.name === studentRoleName)
+          const member = await interaction.guild?.members.fetch(
+            interaction.user.id
+          )
+          if (!member || !guildRole || !schoolRole) {
+            return
           }
-      ],
-      "roles": [
-          "student"
-      ],
-      "promo": {
-          "city": {
-              "code": "FR/STG",
-              "name": "Strasbourg"
-          },
-          "cursus": {
-              "code": "bachelor",
-              "name": "bachelor"
-          },
-          "promotion_year": 2026,
-          "subpromo": "classic"
-      },
-      "is_active": true,
-      "firstname": "Tristan",
-      "lastname": "DISTRETTI"
-    }
-      // attribuer les roles
-      interaction.editReply("Compte vérifié avec succès.")
+          await member.roles.add(guildRole.id)
+          await member.roles.add(schoolRole.id)
+        }
+      }
+      await interaction.editReply("Compte vérifié avec succès.")
     } catch (error) {
       Logger.error("error", `Error while verifying code: ${error}`)
-      interaction.editReply("Erreur lors de la vérification du code.")
+      await interaction.editReply("Erreur lors de la vérification du code.")
     }
   },
 }
--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -1,3 +1,2 @@
 export { default as Student } from "@/database/models/model.student"
 export { default as Config } from "@/database/models/model.config"
-
