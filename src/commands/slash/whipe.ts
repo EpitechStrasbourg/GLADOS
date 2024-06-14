d8a9dcedd23aa1bb3e5a1452c72924997eb028f1
Arthur ROBINE <arthur.robine@gmail.com>
2024-06-14 18:04:11 +0200

    fix: little fix on whipe command

--- src/commands/slash/whipe.ts
+++ src/commands/slash/whipe.ts
@@ -14,22 +14,22 @@ const command: SlashCommand = {
     try {
       await interaction.deferReply({ ephemeral: true });
       await interaction.editReply('Whiping all channels and roles...');
-      const channels = await interaction.guild!.channels.fetch(undefined, {
-        force: true,
-      });
-      channels.forEach(async (channel) => {
-        try {
-          await channel!.delete();
-        } catch (err) {
-          Logger.error(err);
-        }
-      });
       const roles = await interaction.guild!.roles.fetch(undefined, {
         force: true,
       });
       roles.forEach(async (role) => {
         try {
           await role.delete();
+        } catch (err) {
+          Logger.error(err);
+        }
+      });
+      const channels = await interaction.guild!.channels.fetch(undefined, {
+        force: true,
+      });
+      channels.forEach(async (channel) => {
+        try {
+          await channel!.delete();
         } catch (err) {
           Logger.error(err);
         }
