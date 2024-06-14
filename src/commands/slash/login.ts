f4630b4f8d8fefd74094d5452059ff36154686c8
Arthur ROBINE <arthur.robine@gmail.com>
2024-06-14 17:29:50 +0200

    feat/roles-channels-creation: merge with main

--- src/commands/slash/login.ts
+++ src/commands/slash/login.ts
@@ -77,7 +77,7 @@ const command: SlashCommand = {
           discordId: interaction.user.id,
           login: email,
           verificationCode,
-          isVerified: false,
+          verified: false,
         });
       }
 
